e0227ec3a7cac2c6fb66e736d6883245c7c63893
Alexandre Chambriat <alex.cham@yandex.com>
2017-03-04 20:32:20 +0100

    config

--- src/config.h
+++ src/config.h
@@ -2,9 +2,12 @@
 #define __SAMPLER_CONFIG__
 
 #define SAMPLER_DEVICE          "default"  // hw:2,0
+#define SAMPLER_VOICES          128
+#define SAMPLER_BUFFER_SIZE     4096
+
+// hard-fixed
+
 #define SAMPLER_RATE            44100
 #define SAMPLER_CHANNELS        2
-#define SAMPLER_BUFFER_SIZE     4096
-#define SAMPLER_VOICES          128
 
 #endif
