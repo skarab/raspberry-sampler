f8299593430a5fb99281d08bc4c6f27a96b636c8
Alexandre Chambriat <alex.cham@yandex.com>
2017-03-05 18:21:09 +0100

    prepare build on pi

--- Makefile
+++ Makefile
@@ -3,23 +3,23 @@ CXX=g++
 RM=rm -f
 CPPFLAGS=-O3 -s -std=c99
 LDFLAGS=
-LDLIBS=-L/usr/lib -lasound
+LDLIBS=-L/usr/lib -lasound -lpthread -lncurses
 
-SRCS=main.c
+SRCS=main.cpp device.cpp error.cpp sample.cpp voice.cpp wav.cpp
 OBJS=$(subst .c,.o,$(SRCS))
 
-all: synth
+all: sampler
 
-synth: $(OBJS)
-	$(CXX) $(LDFLAGS) -o synth $(OBJS) $(LDLIBS)
+sampler: $(OBJS)
+	$(CXX) $(LDFLAGS) -o sampler $(OBJS) $(LDLIBS)
 
-main.o: main.c
+main.o: main.cpp
 
 clean:
 	$(R%) $(OBJS)
 
 distclean: clean
-	$(RM) synth
+	$(RM) sample
 
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,27 @@
 #include "includes.h"
 #include "device.h"
+#include <ncurses.h>
 
 int main(int argc, char *argv[])
 {
     try
     {
+        initscr();
+        clear();
+        noecho();
+        cbreak();
+        WINDOW* menu_win = newwin(1, 1, 0, 0);
+        keypad(menu_win, TRUE);
+        refresh();
+
         Device* device = new Device(SAMPLER_DEVICE, SAMPLER_RATE, SAMPLER_CHANNELS, SAMPLER_BUFFER_SIZE, SAMPLER_VOICES);
 
         Sample* sample = new Sample("sample.wav");
 
         while(1)
         {
-            char c = getchar();
+            char c = wgetch(menu_win);
             if (c==27) break;
-
-            printf("play\n");
 
             device->Play(sample);
             usleep(10);
@@ -23,6 +30,10 @@ int main(int argc, char *argv[])
         delete sample;
 
         delete device;
+
+        clrtoeol();
+        refresh();
+        endwin();
     }
     catch(Error& error)
     {
