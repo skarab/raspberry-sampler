1882212a5f91df472a8b1a70d784561ecff78dbb
Alexandre Chambriat <alex.cham@yandex.com>
2017-03-06 17:06:43 +0100

    add run scripts

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
     Device* device = new Device(SAMPLER_DEVICE, SAMPLER_RATE, SAMPLER_CHANNELS, SAMPLER_BUFFER_SIZE, SAMPLER_VOICES);
     Midi* midi = new Midi();
 
-    Sample* sample = new Sample("sample.wav");
+    Sample* sample = new Sample("data/sample.wav");
 
     while(1)
     {
