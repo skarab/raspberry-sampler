8a2b1638217fbe397aa56dece9a5a2c6459b112c
Alexandre Chambriat <alex.cham@yandex.com>
2017-03-09 15:51:16 +0100

    remove controller

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,8 @@
 #include "display.h"
 #include "device.h"
 #include "midi.h"
-#include "controller.h"
+#include "button.h"
+#include "knob.h"
 
 int main(int argc, char *argv[])
 {
@@ -10,19 +11,30 @@ int main(int argc, char *argv[])
     Display* display = new Display();
     Device* device = new Device();
     Midi* midi = new Midi();
-    Controller* controller = new Controller();
+
+    #if ENABLE_HARDWARE
+        if (!bcm2835_init())
+            ERROR("failed to initialize GPIO");
+    #endif
+
+    Button* test_button = new Button(RPI_PIN_22);
+    Knob* test_knob = new Knob(255, 200, 280, RPI_PIN_20, RPI_PIN_21);
 
     while(1)
     {
-        char c = getchar();
-        if (c==27) break;
+        test_button->Update();
+        test_knob->Update();
 
-        device->OnNoteOn(0, 0, 0, 0);
-
-        usleep(10);
+        usleep(100);
     }
 
-    delete controller;
+    delete test_knob;
+    delete test_button;
+
+    #if ENABLE_HARDWARE
+        bcm2835_close();
+    #endif
+
     delete midi;
     delete device;
     delete display;
