7964eabb0440e3f399c655bc6e0a50411d6e544a
skarab <a.chambriat@gmail.com>
2017-12-07 20:05:18 +0100

    run directory independant

--- src/config.h
+++ src/config.h
@@ -8,7 +8,7 @@
 #define SAMPLER_PERIOD_SIZE     128
 #define SAMPLER_RATE            44100
 #define SAMPLER_CHANNELS        2
-#define SAMPLER_DISPLAY_PIPE    "python sources/src/display.py"
+#define SAMPLER_DISPLAY_PIPE    "python /sampler/sources/src/display.py"
 #define SAMPLER_USB_KEY_DEVICE  "/dev/disk/by-label/"
 #define SAMPLER_USB_KEY_MOUNT   "/mnt/usb"
 #define SAMPLER_FOLDER          "/mnt/usb/samples/"
